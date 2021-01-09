8d2ad200eb71b0d0b8ca1efb553b9f556a982aa3
Matthew Cook <matthewcook13987@gmail.com>
2021-01-09 22:18:22 +0000

    Added whichSchool.js practice file for conditional functions

---
+++ whichSchool.js
@@ -0,0 +1,29 @@
+/*
+Elementary School if age is below 13
+Secondary School if age is between 13 and 18(both inclusive)
+Lighthouse Labs in all other cases.
+*/
+
+/*
+function whichSchool(age) {
+  if (age <= 13) {
+    console.log("Elementary School")
+  } else if (age > 13 && age < 18) {
+    console.log("Secondary School")
+  } else if (age > 18){
+    console.log("Lighthouse Labs")
+  }
+}
+*/
+
+function whichSchool(age) {
+  if (age <= 12) {
+    console.log("Elementary School")
+  } else if (age >= 13 && age <= 18) {
+    console.log("Secondary School")
+  } else {
+    console.log("Lighthouse Labs")
+  }
+}
+
+whichSchool(18)
