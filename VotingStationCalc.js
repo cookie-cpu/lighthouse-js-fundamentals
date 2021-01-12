2753a2470fe497d90d075b06ab06a6eb282177ee
Matthew Cook <matthewcook13987@gmail.com>
2021-01-12 21:56:52 +0000

    Added  Voting Station calculator file

---
+++ VotingStationCalc.js
@@ -0,0 +1,23 @@
+/*
+chooseStations(stations) function
+takes in an array of possible voting stations
+then only returns the names of the stations that are appropriate
+
+*/
+
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+
+
+function chooseStations(){
+
+}
+
+
+
+//chooseStations(stations);
