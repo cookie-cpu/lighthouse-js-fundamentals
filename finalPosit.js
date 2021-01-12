8a530c2c0c497c850163f80166bd40bd8c6b8d8b
Matthew Cook <matthewcook13987@gmail.com>
2021-01-12 22:24:37 +0000

    Added commented notes to help plan the program

---
+++ finalPosit.js
@@ -0,0 +1,23 @@
+/*
+
+takes moves[n,s,w,e]
+start position [0,0]
+return final position [-1,4]
+north = y++
+south = y--
+east = x--
+west = x++
+
+
+*/
+
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+
+function finalPosition(moves){
+
+}
+
+
+//finalPosition(moves);
