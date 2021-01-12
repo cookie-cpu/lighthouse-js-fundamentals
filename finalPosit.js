6565307aa8d68ffdbd61ee88d5261e7b9174c66f
Matthew Cook <matthewcook13987@gmail.com>
2021-01-12 22:29:33 +0000

    added a for loop which iterates over the 'moves' array

--- finalPosit.js
+++ finalPosit.js
@@ -12,12 +12,17 @@ west = x++
 */
 
 
-const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'] ;
+
 
 
 function finalPosition(moves){
+  const position = [0,0];
 
+  for (var move in moves){
+    console.log(moves[move])
+  }
 }
 
 
-//finalPosition(moves);
+finalPosition(moves);
