b3a495b1e1c8c271a24ab16f103cc830a6a9c41c
Matthew Cook <matthewcook13987@gmail.com>
2021-01-12 23:21:10 +0000

    Added test cases to check for functionality

---
+++ howManyHundreds.js
@@ -0,0 +1,12 @@
+function howManyHundreds(num){
+return num%10
+}
+
+//console.log(howManyHundreds(894))
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
