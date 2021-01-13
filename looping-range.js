d5ef4e49ad4601ec56cbf4a6927861727579bb51
Matthew Cook <matthewcook13987@gmail.com>
2021-01-13 21:56:05 +0000

    Created looping-range.js including instructions and test cases

---
+++ looping-range.js
@@ -0,0 +1,31 @@
+/*
+
+//TODO
+// Define function range
+// Function takes 3 params (start, end, step)
+// The function should return an array of numbers from start to end counting by step
+
+The function should return an empty array [] if given incorrect parameters, such as:
+
+    start, end, or step being undefined
+    start being greater than end
+    step being 0, or negative
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+//Test Cases
+/*
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+*/
