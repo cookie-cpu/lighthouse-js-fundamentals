dec7a392083353a0373e6904bd9944a7619a1c6b
Matthew Cook <matthewcook13987@gmail.com>
2021-01-13 21:45:02 +0000

    Created first 2 loops for array iteration and commented on their processes.

---
+++ ingredients.js
@@ -0,0 +1,52 @@
+/*
+    Write a loop under each comment in the file.
+    Under the first, write a while loop that prints out each item of ingredients.
+    Under the second, write a for loop that does the same thing.
+    And under the third, write any loop, while or for that prints out each item of ingredients but backwards
+    (that is, the first should be "bananas" and the last "eggs").
+*/
+
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"]; //Array declaration
+
+
+// Write a while loop that prints out the contents of ingredients:
+
+let i = 0;                                    //creates an index for loop condition
+while (i < ingredients.length) {              //condition checks if index is higher than number of array items
+  console.log(ingredients[i]);                 // prints the array item at the index to the console
+  i++;                                       //increases the index
+}
+
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (i = 0; i <= ingredients.length; i++) {
+  console.log(ingredients[i])
+
+}
+
+
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+
+
+
+
+/*
+
+//Previous Attempts
+
+let x = 0;
+while (x < ingredients.length) {
+  reverse = ingredients.reverse()
+  
+} console.log(reverse[i])
+
+
+for (i = 7; i >= ingredients.length; i--) {
+  console.log(ingredients[i])
+
+}
+*/
+
