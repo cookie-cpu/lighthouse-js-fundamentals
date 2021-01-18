c821a1b00a92cb98d3a3fa47a8720cb163cb43c8
Matthew Cook <matthewcook13987@gmail.com>
2021-01-18 18:28:42 +0000

    added conditional logic to return the empty array if the arguments are ill-defined

--- looping-range.js
+++ looping-range.js
@@ -10,12 +10,30 @@ The function should return an empty array [] if given incorrect parameters, such
     start, end, or step being undefined
     start being greater than end
     step being 0, or negative
-
-
 */
+
+
+
+
 let array = [];
 
-function range (start, end, step){
+function range(start, end, step) {
+
+    if (start === undefined || end === undefined || step === undefined) {
+        return array
+    } else if (start > end){
+        return array
+    } else if (step <=0){
+        return arr
+    } else {
+        for (i=0; i<= end; i++){
+
+
+
+
+            
+        }
+    }
 
 }
 
@@ -27,7 +45,8 @@ function range (start, end, step){
 
 //Test Cases
 /*
-console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+console.log(range()) //output undefined
+console.log(range(0, 10, 2)); //output [ 0, 2, 4, 6, 8, 10 ]
+console.log(range(10, 30, 5)); //output [ 10, 15, 20, 25, 30 ]
+console.log(range(-5, 2, 3));   //output [ -5, -2, 1 ]
 */
