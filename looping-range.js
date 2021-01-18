320fa9a522c7285ac84d13db758dbd284820c3b9
Matthew Cook <matthewcook13987@gmail.com>
2021-01-18 18:51:57 +0000

    changed the for loop to a while loop that uses the range() functions arguments instead of an index

--- looping-range.js
+++ looping-range.js
@@ -1,5 +1,4 @@
 /*
-
 //TODO
 // Define function range
 // Function takes 3 params (start, end, step)
@@ -12,9 +11,9 @@ The function should return an empty array [] if given incorrect parameters, such
     step being 0, or negative
 */
 
-let array = [];
-function range(start, end, step) {
 
+function range(start, end, step) {
+    let array = [];
     if (start === undefined || end === undefined || step === undefined) {
         return array
     } else if (start > end) {
@@ -22,25 +21,19 @@ function range(start, end, step) {
     } else if (step <= 0) {
         return array
     } else {
-        for (i = 0; i <= end; i++) {
+        while (start <= end) {
             array.push(start)
             start += step
-
-
         } return array
     }
-
 }
 
-
 //Test Cases
-
-
 /*
+
+*/
 
 console.log(range()) //output undefined
 console.log(range(0, 10, 2)); //output [ 0, 2, 4, 6, 8, 10 ]
 console.log(range(10, 30, 5)); //output [ 10, 15, 20, 25, 30 ]
 console.log(range(-5, 2, 3));   //output [ -5, -2, 1 ]
-
-*/
