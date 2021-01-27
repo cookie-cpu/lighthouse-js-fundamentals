819288650df31f5ee08431efae3c595e8ea614b4
Matthew Cook <matthewcook13987@gmail.com>
2021-01-27 01:18:01 +0000

    created sumLargestNumbers.js file including a starting function declaration, todo list and test cases.

---
+++ sumLargestNumbers.js
@@ -0,0 +1,18 @@
+/*
+given an array of 2 or more numbers
+find the two largest numbers in that array
+sum them together
+*/
+
+const sumLargestNumbers = function(data) {
+  for (num in data){
+    console.log(num)
+  }
+};
+
+
+
+//TEST CASES
+console.log(sumLargestNumbers([1, 10]));
+//console.log(sumLargestNumbers([1, 2, 3]));
+//console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
