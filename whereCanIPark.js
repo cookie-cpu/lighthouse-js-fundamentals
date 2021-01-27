9f53ac1b5e0c0a20fe9b0142c945e0961b7d5f0e
Matthew Cook <matthewcook13987@gmail.com>
2021-01-27 21:34:57 +0000

    finished the whereCanIPark,js file

--- whereCanIPark.js
+++ whereCanIPark.js
@@ -1,7 +1,29 @@
-const whereCanIPark = function (spots, vehicle) {
-  // Code here!
-};
+const whereCanIPark = function (spots, vehicle){
 
+  for (let y = 0; y < spots.length; y++){
+
+    for (let x = 0; x < spots[y].length; x++){
+
+      const spot = spots[y][x]
+
+      if (vehicle === 'regular'){
+        if (spot === 'R'){
+          return [x,y]
+        }
+      } else if (vehicle === 'small'){
+        if (spot === 'R' || spot === 'S'){
+          return [x,y]
+        }
+      } else if (vehicle === 'motorcycle'){
+        if (spot === 'R' || spot === 'S' || spot === 'M'){
+          return [x,y]
+        }
+      }
+    }
+  } return false
+}
+
+//TEST CASES
 console.log(whereCanIPark(
   [
     // COLUMNS ARE X
