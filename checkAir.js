459f1218f50d5b55abef622dbf83b4a5a5208891
Matthew Cook <matthewcook13987@gmail.com>
2021-01-27 21:58:38 +0000

    created checkair.js

---
+++ checkAir.js
@@ -0,0 +1,32 @@
+const checkAir = function (samples, threshold) {
+
+  let dirtyNum = 0;
+  
+  for (aircheck in samples){
+    if (samples[aircheck] === 'dirty'){
+      dirtyNum++;
+    }
+  } if (threshold < dirtyNum/10){
+    return 'Polluted'
+  } else if (threshold > dirtyNum/10){
+    return 'Clean'
+  }
+}
+
+
+
+//TEST CASES
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
