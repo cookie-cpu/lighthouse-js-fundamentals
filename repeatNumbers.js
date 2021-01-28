671dd46b26c068dae7b981e826a898cf346c330f
Matthew Cook <matthewcook13987@gmail.com>
2021-01-28 17:02:29 +0000

    initialzed repeatNumbers.js with test cases, function declaration and todo list/psuedocode for program functionallity

---
+++ repeatNumbers.js
@@ -0,0 +1,27 @@
+/*
+input is two dimensional array
+ (an array of arrays), where each sub-array will have two numeric values. 
+ For example:
+[[1, 2], [2, 3]]
+
+The first will be the value to repeat, 
+the second will be the amount of times to repeat that value.
+
+//TODO
+init string val
+loop through arrays
+set first num as factor
+print second num as many times as factor
+*/
+
+
+const repeatNumbers = function(data) {
+  // Put your solution here
+};
+
+
+
+//TEST CASES
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
