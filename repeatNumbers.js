1f3b313d71e1ff733c316de247534a1be70616cf
Matthew Cook <matthewcook13987@gmail.com>
2021-02-05 01:17:57 +0000

    Initial commit of casemaker.js

--- repeatNumbers.js
+++ repeatNumbers.js
@@ -1,27 +1,22 @@
-/*
-input is two dimensional array
- (an array of arrays), where each sub-array will have two numeric values. 
- For example:
-[[1, 2], [2, 3]]
-
-The first will be the value to repeat, 
-the second will be the amount of times to repeat that value.
-
-//TODO
-init string val
-loop through arrays
-set first num as factor
-print second num as many times as factor
-*/
-
-
 const repeatNumbers = function(data) {
-  // Put your solution here
+  let output = ''; //inits string so vals are concat not summed
+  for (let i = 0; i < data.length; i++){ //loops through array elements
+    if (i > 0){
+      output += ', '; //creates a comma and space after each where the index is 0 again
+    }
+    for (let j = 0; j < data[i][1]; j++){ //Loops through as many times as the second array value dictates
+      output += data[i][0] //Pushes the value to the string each loop
+    } //console.log(data[i][i])
+  } return output;
 };
 
 
-
 //TEST CASES
-console.log(repeatNumbers([[1, 10]]));
-console.log(repeatNumbers([[1, 2], [2, 3]]));
-console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[1, 10]])); // => 1111111111
+console.log(repeatNumbers([[1, 2], [2, 3]])); // => 11, 222
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]])); // => 10101010, 343434343434, 9292
+
+ 
+
+
+
