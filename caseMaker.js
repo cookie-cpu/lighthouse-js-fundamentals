3ef1af4f7b669b73f52e6080741e3b9903be4ba7
Matthew Cook <matthewcook13987@gmail.com>
2021-02-05 01:31:08 +0000

    implemented casemaker.js

---
+++ caseMaker.js
@@ -0,0 +1,17 @@
+const camelCase = function(input) {
+  let output = "";
+  for (let i = 0; i < input.length; i++){
+    if (input[i] === " "){
+      output += input[i+1].toUpperCase();
+      i++;
+    } else {
+      output += input[i]
+    }
+  } return output  
+};
+
+console.log(camelCase("this is a string")); //thisIsAString
+console.log(camelCase("loopy lighthouse")); //loopyLighthouse
+console.log(camelCase("supercalifragalisticexpialidocious")); //supercalifragalisticexpialidocious
+console.log(camelCase("this should be in camel case")); //thisShouldBeInCamelCase
+
