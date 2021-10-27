22708c0b1497c0be274fb4909fbc137db8fab201
Matthew Cook <matthewcook13987@gmail.com>
2021-10-27 13:12:33 -0500

    adds kata folder

---
+++ katas/10.js
@@ -0,0 +1,24 @@
+/*
+Multiplication Table
+
+We will be given a number as our input data. 
+This number is the highest value of our multiplication table. 
+Our job is to generate a multiplication table for the values from 1 to the provided number.
+
+
+
+Create a function named multiplicationTable that receives a number maxValue as input 
+
+and creates a square multiplication table where maxValue is the largest value in the table.
+*/
+
+
+
+const multiplicationTable = function(maxValue) {
+  let arr
+  // Your code here
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
